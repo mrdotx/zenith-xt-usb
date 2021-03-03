77d825302f7364a304b695c8d1553fa0a4a4f866
mrdotx <klassiker@gmx.de>
2021-03-03 14:14:45 +0100

    macro structure changed

--- unimap_plain.c
+++ unimap_plain.c
@@ -2,7 +2,7 @@
  * path:   /home/klassiker/.local/share/repos/zenith-xt-usb/unimap_plain.c
  * author: klassiker [mrdotx]
  * github: https://github.com/mrdotx/zenith-usb
- * date:   2021-03-03T10:06:18+0100
+ * date:   2021-03-03T14:12:19+0100
  */
 
 #include "unimap_trans.h"
@@ -62,72 +62,70 @@ const action_t actionmaps[][UNIMAP_ROWS][UNIMAP_COLS] PROGMEM = {
 };
 
 /* macro definitions */
+#define C_INTERVAL  I(5)
+
+#define O_TERMINAL  D(LGUI), T(ENT), U(LGUI), W(250)
+#define O_WEB       D(LGUI), T(W), U(LGUI), W(100)
+#define O_TOP       D(LGUI), T(T), U(LGUI), W(100)
+#define O_DESK2     D(LGUI), T(2), U(LGUI), W(100)
+#define O_LAYOUT    D(LGUI), T(SPC), U(LGUI), T(LEFT), T(LEFT), T(LEFT), T(ENT), W(100)
+#define O_FOCUS     D(LGUI), T(LEFT), U(LGUI), W(100)
+#define O_PI        D(LGUI), T(H), U(LGUI), W(100)
+#define O_PI2       D(LGUI), D(LSFT), T(H), U(LSFT), U(LGUI), W(100)
+
+#define T_CLEAR     T(SPC), T(C), T(L), T(E), T(A), T(R)
+#define T_EXIT      T(SPC), T(E), T(X), T(I), T(T)
+#define T_RANGER    T(SPC), T(R), T(A), T(N), T(G), T(E), T(R)
+#define T_CINFO     T(SPC), T(C), T(I), T(N), T(F), T(O)
+#define T_XSET      T(SPC), T(X), T(S), T(E), T(T), T(SPC), \
+                    T(R), T(SPC), \
+                    T(R), T(A), T(T), T(E), T(SPC), \
+                    T(2), T(0), T(0), T(SPC), \
+                    T(5), T(0)
+#define T_SETXKBMAP T(SPC), T(S), T(E), T(T), T(X), T(K), T(B), T(M), T(A), T(P), T(SPC), \
+                    T(MINS), T(M), T(O), T(D), T(E), T(L), T(SPC), \
+                    T(P), T(C), T(1), T(0), T(5), T(SPC), \
+                    T(MINS), T(L), T(A), T(Y), T(O), T(U), T(T), T(SPC), \
+                    T(U), T(S), T(COMM), T(D), T(E), T(SPC), \
+                    T(MINS), T(O), T(P), T(T), T(I), T(O), T(N), T(SPC), \
+                    T(G), T(R), T(P), D(LSFT), T(SCLN), U(LSFT), T(C), T(A), T(P), T(S), D(LSFT), T(MINS), U(LSFT), T(S), T(W), T(I), T(T), T(C), T(H)
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
     switch (id) {
         case SETKB:
             return (record->event.pressed ?
                     MACRO( \
-                        /* set interval */
-                        I(5), \
-                        /* open terminal and wait for x milliseconds */
-                        D(LGUI), T(ENT), U(LGUI), W(250), W(250), \
-                        /* type ' xset r rate 200 50;' */
-                        T(SPC), \
-                        T(X), T(S), T(E), T(T), T(SPC), \
-                        T(R), T(SPC), \
-                        T(R), T(A), T(T), T(E), T(SPC), \
-                        T(2), T(0), T(0), T(SPC), \
-                        T(5), T(0), T(SCLN), \
-                        /* type ' setxkbmap -model pc105 -layout us,de -option grp:caps_switch;' */
-                        T(SPC),
-                        T(S), T(E), T(T), T(X), T(K), T(B), T(M), T(A), T(P), T(SPC), \
-                        T(MINS), T(M), T(O), T(D), T(E), T(L), T(SPC), \
-                        T(P), T(C), T(1), T(0), T(5), T(SPC), \
-                        T(MINS), T(L), T(A), T(Y), T(O), T(U), T(T), T(SPC), \
-                        T(U), T(S), T(COMM), T(D), T(E), T(SPC), \
-                        T(MINS), T(O), T(P), T(T), T(I), T(O), T(N), T(SPC), \
-                        T(G), T(R), T(P), D(LSFT), T(SCLN), U(LSFT), T(C), T(A), T(P), T(S), D(LSFT), T(MINS), U(LSFT), T(S), T(W), T(I), T(T), T(C), T(H), T(SCLN), \
-                        /* type ' exit' and press enter (close terminal) */
-                        T(SPC), \
-                        T(E), T(X), T(I), T(T), T(ENT), \
+                        /* set typing interval */
+                        C_INTERVAL, \
+                        /* open terminal */
+                        O_TERMINAL, \
+                        /* execute xset, setxkmap and close terminal */
+                        T_XSET, T(SCLN), T_SETXKBMAP, T(SCLN), T_EXIT, T(ENT), \
                         END ) :
                     MACRO_NONE );
         case AUTOSTART:
             return (record->event.pressed ?
                     MACRO( \
-                        /* set interval */
-                        I(5), \
-                        /* open webbrowser and wait for x milliseconds*/
-                        D(LGUI), T(W), U(LGUI), W(100), \
-                        /* open system monitor */
-                        D(LGUI), T(T), U(LGUI), W(100), \
-                        /* go to desktop 2 */
-                        D(LGUI), T(2), U(LGUI), W(100), \
-                        /* open terminal and wait for x milliseconds */
-                        D(LGUI), T(ENT), U(LGUI), W(250), W(250), \
-                        /* type ' ranger' and press enter */
-                        T(SPC), \
-                        T(R), T(A), T(N), T(G), T(E), T(R), T(ENT), W(100), \
-                        /* open another terminal and wait for x milliseconds */
-                        D(LGUI), T(ENT), U(LGUI), W(250), \
-                        /* type ' cinfo', press enter and wait for x milliseconds*/
-                        T(SPC), \
-                        T(C), T(I), T(N), T(F), T(O), T(ENT), W(100), \
-                        /* change terminal layout, wait for x milliseconds and change focus */
-                        D(LGUI), T(SPC), U(LGUI), T(LEFT), T(LEFT), T(LEFT), T(ENT), W(100), \
-                        D(LGUI), T(LEFT), U(LGUI), \
+                        /* set typing interval */
+                        C_INTERVAL, \
+                        /* open webbrowser, system monitor and go to desktop 2 */
+                        O_WEB, O_TOP, O_DESK2, \
+                        /* open terminal, clear terminal and execute ranger */
+                        O_TERMINAL, T_CLEAR, T(SCLN), T_RANGER, T(ENT), W(100), \
+                        /* open another terminal, clear terminal and execute cinfo */
+                        O_TERMINAL, T_CLEAR, T(SCLN), T_CINFO, T(ENT), W(100), \
+                        /* change terminal layout and window focus */
+                        O_LAYOUT, O_FOCUS, \
                         END ) :
                     MACRO_NONE );
         case OPENSSH:
             return (record->event.pressed ?
                     MACRO( \
-                        /* set interval */
-                        I(5), \
-                        /* open ssh to hermes */
-                        D(LGUI), T(H), U(LGUI), W(100), \
-                        /* open ssh to prometheus */
-                        D(LGUI), D(LSFT), T(H), U(LSFT), U(LGUI), \
+                        /* set typing interval */
+                        C_INTERVAL, \
+                        /* open ssh to hermes and prometheus */
+                        O_PI, O_PI2, \
                         END ) :
                     MACRO_NONE );
     }
