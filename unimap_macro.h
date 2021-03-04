0ac13344b25e46b5ca5890e2fec03c6f93107a2e
mrdotx <klassiker@gmx.de>
2021-03-04 15:12:04 +0100

    changed reboot and keyboard options macros to use dmenu

--- unimap_macro.h
+++ unimap_macro.h
@@ -1,14 +1,15 @@
-/*
+/*:
  * path:   /home/klassiker/.local/share/repos/zenith-xt-usb/unimap_macro.h
  * author: klassiker [mrdotx]
  * github: https://github.com/mrdotx/zenith-usb
- * date:   2021-03-04T14:00:01+0100
+ * date:   2021-03-04T15:09:12+0100
  */
 
 #define C_INTERVAL  I(5)
 #define C_WAIT      W(100)
 
 #define O_TERMINAL  D(LGUI), T(ENT), U(LGUI), W(255)
+#define O_DMENU     D(LSFT), T(LGUI), U(LSFT), C_WAIT
 #define O_WEB       D(LGUI), T(W), U(LGUI), C_WAIT
 #define O_TOP       D(LGUI), T(T), U(LGUI), C_WAIT
 #define O_DESK2     D(LGUI), T(2), U(LGUI), C_WAIT
@@ -17,11 +18,21 @@
 #define O_PI        D(LGUI), T(H), U(LGUI), C_WAIT
 #define O_PI2       D(LGUI), D(LSFT), T(H), U(LSFT), U(LGUI), C_WAIT
 
-#define T_EXIT      T(E), T(X), T(I), T(T), T(ENT), C_WAIT
 #define T_REBOOT    T(R), T(E), T(B), T(O), T(O), T(T), T(ENT), C_WAIT
 #define T_RANGER    T(R), T(G), T(ENT), C_WAIT
 #define T_CINFO     T(C), T(L), T(ENT), C_WAIT
-#define T_KEYBOARD  T(K), T(B), T(ENT), C_WAIT
+#define T_XSET      T(X), T(S), T(E), T(T), T(SPC), \
+                    T(R), T(SPC), \
+                    T(R), T(A), T(T), T(E), T(SPC), \
+                    T(2), T(0), T(0), T(SPC), \
+                    T(5), T(0), T(ENT), C_WAIT
+#define T_SETXKBMAP T(S), T(E), T(T), T(X), T(K), T(B), T(M), T(A), T(P), T(SPC), \
+                    T(MINS), T(M), T(O), T(D), T(E), T(L), T(SPC), \
+                    T(P), T(C), T(1), T(0), T(5), T(SPC), \
+                    T(MINS), T(L), T(A), T(Y), T(O), T(U), T(T), T(SPC), \
+                    T(U), T(S), T(COMM), T(D), T(E), T(SPC), \
+                    T(MINS), T(O), T(P), T(T), T(I), T(O), T(N), T(SPC), \
+                    T(G), T(R), T(P), D(LSFT), T(SCLN), U(LSFT), T(C), T(A), T(P), T(S), D(LSFT), T(MINS), U(LSFT), T(S), T(W), T(I), T(T), T(C), T(H), T(ENT), C_WAIT
 
 #define AC_M0       ACTION_MACRO(SETKB)
 #define AC_M1       ACTION_MACRO(AUTOSTART)
@@ -43,8 +54,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                     MACRO( \
                         /* set typing interval */
                         C_INTERVAL, \
-                        /* open terminal, set keyboard options and close terminal */
-                        O_TERMINAL, T_KEYBOARD, T_EXIT, \
+                        /* open dmenu and set keyboard options */
+                        O_DMENU, T_XSET, O_DMENU, T_SETXKBMAP, \
                         END ) :
                     MACRO_NONE );
         case AUTOSTART:
@@ -76,8 +87,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                     MACRO( \
                         /* set typing interval */
                         C_INTERVAL, \
-                        /* open terminal and execute reboot */
-                        O_TERMINAL, T_REBOOT, \
+                        /* open dmenu and reboot */
+                        O_DMENU, T_REBOOT, \
                         END ) :
                     MACRO_NONE );
     }
