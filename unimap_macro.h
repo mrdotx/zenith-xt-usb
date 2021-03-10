81a58ad8acc3f4e7f6d353206f98bd0ed2af1ad2
mrdotx <klassiker@gmx.de>
2021-03-10 22:55:41 +0100

    macro neofetch added

--- unimap_macro.h
+++ unimap_macro.h
@@ -1,18 +1,19 @@
-/*:
+/*
  * path:   /home/klassiker/.local/share/repos/zenith-xt-usb/unimap_macro.h
  * author: klassiker [mrdotx]
  * github: https://github.com/mrdotx/zenith-usb
- * date:   2021-03-10T15:52:12+0100
+ * date:   2021-03-10T22:50:43+0100
  */
 
 #define AC_KB       ACTION_MACRO(SETKB)
 #define AC_AUTO     ACTION_MACRO(AUTOSTART)
 #define AC_SSH      ACTION_MACRO(OPENSSH)
 #define AC_BOOT     ACTION_MACRO(REBOOT)
+#define AC_VENT     ACTION_MACRO(VENTOY)
 #define AC_WT       ACTION_MACRO(WEATHER)
 #define AC_CV       ACTION_MACRO(COVID)
+#define AC_NEOF     ACTION_MACRO(NEOFETCH)
 #define AC_TCOL     ACTION_MACRO(TERMCOLORS)
-#define AC_VENT     ACTION_MACRO(VENTOY)
 
 #define C_INTERVAL  I(5)
 #define C_WAIT_DS   W(100)
@@ -32,23 +33,9 @@
 
 #define T_REBOOT    T(R), T(E), T(B), T(O), T(O), T(T), T(ENT), C_WAIT_DS
 #define T_RANGER    T(R), T(G), T(ENT), C_WAIT_QS, C_WAIT_QS, T(QUOT), T(R), C_WAIT_DS
-#define T_WEATHER   T(W), T(T), T(ENT), C_WAIT_DS
-#define T_COVID     T(C), T(V), T(ENT), C_WAIT_DS
 #define T_CINFO     T(C), T(I), T(N), T(F), T(O), T(ENT), C_WAIT_DS
+#define T_NEOFETCH  T(N), T(E), T(O), T(F), T(E), T(T), T(C), T(H), T(ENT), C_WAIT_DS
 
-/* terminal_colors.sh */
-#define T_TERMCOLORS    \
-    T(T), T(E), T(R), T(M), T(I), T(N), T(A), T(L), D(LSFT), T(MINS), U(LSFT), \
-    T(C), T(O), T(L), T(O), T(R), T(S), T(DOT), T(S), T(H), T(ENT), C_WAIT_DS
-/* doas ventoyweb */
-#define T_VENTOY    \
-    T(D), T(O), T(A), T(S), T(SPC), \
-    T(V), T(E), T(N), T(T), T(O), T(Y), T(W), T(E), T(B), T(ENT), C_WAIT_DS
-/* surf.sh 127.0.0.1:24680 */
-#define T_VENTOYWEB \
-    T(S), T(U), T(R), T(F), T(DOT), T(S), T(H), T(SPC), \
-    T(1), T(2), T(7), T(DOT), T(0), T(DOT), T(0), T(DOT), T(1), D(LSFT), T(SCLN), U(LSFT), \
-    T(2), T(4), T(6), T(8), T(0), T(ENT), C_WAIT_DS
 /* xset r rate 200 50; */
 /* setxkbmap -model pc105 -layout us,de -option grp:caps_switch */
 #define T_KEYBOARD  \
@@ -66,16 +53,48 @@
     T(G), T(R), T(P), D(LSFT), T(SCLN), U(LSFT), \
     T(C), T(A), T(P), T(S), D(LSFT), T(MINS), U(LSFT), \
     T(S), T(W), T(I), T(T), T(C), T(H), T(ENT), C_WAIT_DS
+/* doas ventoyweb */
+#define T_VENTOY    \
+    T(D), T(O), T(A), T(S), T(SPC), \
+    T(V), T(E), T(N), T(T), T(O), T(Y), T(W), T(E), T(B), T(ENT), C_WAIT_DS
+/* surf.sh 127.0.0.1:24680 */
+#define T_VENTOYWEB \
+    T(S), T(U), T(R), T(F), T(DOT), T(S), T(H), T(SPC), \
+    T(1), T(2), T(7), T(DOT), T(0), T(DOT), T(0), T(DOT), T(1), D(LSFT), T(SCLN), U(LSFT), \
+    T(2), T(4), T(6), T(8), T(0), T(ENT), C_WAIT_DS
+/* curl -H "Accept-Language: de" wttr.in */
+#define T_WEATHER   \
+    T(C), T(U), T(R), T(L), T(SPC), \
+    T(MINS), D(LSFT), T(H), U(LSFT), T(SPC), \
+    D(LSFT), T(QUOT), T(A), U(LSFT), T(C), T(C), T(E), T(P), T(T), T(MINS), \
+    D(LSFT), T(L), U(LSFT), T(A), T(N), T(G), T(U), T(A), T(G), T(E), \
+    D(LSFT), T(SCLN), U(LSFT), T(SPC), T(D), T(E), D(LSFT), T(QUOT), U(LSFT), T(SPC), \
+    T(W), T(T), T(T), T(R), T(DOT), T(I), T(N), T(ENT), C_WAIT_DS
+/* curl "https://corona-stats.online?top=25&source=2&minimal=true" */
+#define T_COVID     \
+    T(C), T(U), T(R), T(L), T(SPC), \
+    D(LSFT), T(QUOT), U(LSFT), T(H), T(T), T(T), T(P), T(S), D(LSFT), T(SCLN), U(LSFT), \
+    D(SLSH), D(SLSH), T(C), T(O), T(R), T(O), T(N), T(A), T(MINS), \
+    T(S), T(T), T(A), T(T), T(S), T(DOT), T(O), T(N), T(L), T(I), T(N), T(E), \
+    D(LSFT), T(SLSH), T(SLSH), U(LSFT), T(T), T(O), T(P), T(EQL), T(2), T(5), \
+    D(LSFT), T(7), U(LSFT), T(S), T(O), T(U), T(R), T(C), T(E), T(EQL), T(2), \
+    D(LSFT), T(7), U(LSFT), T(M), T(I), T(N), T(I), T(M), T(A), T(L), T(EQL), \
+    T(T), T(R), T(U), T(E), D(LSFT), T(QUOT), U(LSFT), T(ENT), C_WAIT_DS
+/* terminal_colors.sh */
+#define T_TERMCOLORS    \
+    T(T), T(E), T(R), T(M), T(I), T(N), T(A), T(L), D(LSFT), T(MINS), U(LSFT), \
+    T(C), T(O), T(L), T(O), T(R), T(S), T(DOT), T(S), T(H), T(ENT), C_WAIT_DS
 
 enum macro_id {
     SETKB,
     AUTOSTART,
     OPENSSH,
     REBOOT,
+    VENTOY,
     WEATHER,
     COVID,
+    NEOFETCH,
     TERMCOLORS,
-    VENTOY,
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -123,6 +142,17 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                         O_DMENU, T_REBOOT, \
                         END ) :
                     MACRO_NONE );
+        case VENTOY:
+            return (record->event.pressed ?
+                    MACRO( \
+                        /* set typing interval */
+                        C_INTERVAL, \
+                        /* open terminal and start ventoy server */
+                        O_TERM, T_CLEAR, T_VENTOY, \
+                        /* open dmenu and go to url with surf */
+                        O_DMENU, T_VENTOYWEB, \
+                        END ) :
+                    MACRO_NONE );
         case WEATHER:
             return (record->event.pressed ?
                     MACRO( \
@@ -141,6 +171,15 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                         O_TERMFLOAT, T_CLEAR, T_COVID, \
                         END ) :
                     MACRO_NONE );
+        case NEOFETCH:
+            return (record->event.pressed ?
+                    MACRO( \
+                        /* set typing interval */
+                        C_INTERVAL, \
+                        /* open floating terminal with neofetch info */
+                        O_TERMFLOAT, T_CLEAR, T_NEOFETCH, \
+                        END ) :
+                    MACRO_NONE );
         case TERMCOLORS:
             return (record->event.pressed ?
                     MACRO( \
@@ -148,17 +187,6 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
                         C_INTERVAL, \
                         /* open floating terminal with color info */
                         O_TERMFLOAT, T_CLEAR, T_TERMCOLORS, \
-                        END ) :
-                    MACRO_NONE );
-        case VENTOY:
-            return (record->event.pressed ?
-                    MACRO( \
-                        /* set typing interval */
-                        C_INTERVAL, \
-                        /* open terminal and start ventoy server */
-                        O_TERM, T_CLEAR, T_VENTOY, \
-                        /* open dmenu and go to url with surf */
-                        O_DMENU, T_VENTOYWEB, \
                         END ) :
                     MACRO_NONE );
     }
