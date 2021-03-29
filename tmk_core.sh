6f54dd92d770e47c87fade36ca207c185f863978
mrdotx <klassiker@gmx.de>
2021-03-29 13:23:56 +0200

    typos corrected

--- tmk_core.sh
+++ tmk_core.sh
@@ -3,19 +3,19 @@
 # path:   /home/klassiker/.local/share/repos/zenith-xt-usb/tmk_core.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/terminal-colors
-# date:   2021-03-29T13:07:24+0200
+# date:   2021-03-29T13:22:01+0200
 
 clear
 rm -rf tmk_core
 
-printf ':: git clone tmk_core and remove git files/folders\n'
+printf ':: git clone tmk_core and remove .git files/folders\n'
 git clone git@github.com:tmk/tmk_core.git
 rm -rf tmk_core/.git tmk_core/.gitignore tmk_core/.gitmodules
 
-printf '\n:: git clone submodule lufa and remove git files/folders\n'
+printf '\n:: git clone submodule lufa and remove .git files/folders\n'
 git clone git@github.com:abcminiuser/lufa.git tmk_core/protocol/lufa/lufa-abcminiuser
 rm -rf tmk_core/protocol/lufa/lufa-abcminiuser/.git tmk_core/protocol/lufa/lufa-abcminiuser/.gitignore
 
-printf '\n:: git clone submodule usb host shield 2.0 and remove git files/folders\n'
+printf '\n:: git clone submodule usb host shield 2.0 and remove .git files/folders\n'
 git clone git@github.com:tmk/USB_Host_Shield_2.0.git tmk_core/protocol/usb_hid/USB_Host_Shield_2.0-tmk
 rm -rf tmk_core/protocol/usb_hid/USB_Host_Shield_2.0-tmk/.git tmk_core/protocol/usb_hid/USB_Host_Shield_2.0-tmk/.github tmk_core/protocol/usb_hid/USB_Host_Shield_2.0-tmk/.gitattributes tmk_core/protocol/usb_hid/USB_Host_Shield_2.0-tmk/.gitignore tmk_core/protocol/usb_hid/USB_Host_Shield_2.0-tmk/.gitmodules
